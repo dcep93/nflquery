f036238254c7ba689ff2d9b28621e5e937e07ad5
dcep93 <dcep93@gmail.com>
2025-01-20 04:35:54 -0500

    x

--- .github/workflows/build_react.sh
+++ .github/workflows/build_react.sh
@@ -3,9 +3,10 @@
 set -euo pipefail
 
 # npx create-react-app app --template typescript
-# npm install --save-dev ajv@^7
 
 cd app
+npm config set legacy-peer-deps true
 npm install
+npm install --save-dev ajv@^7
 yarn build
 rm -rf node_modules
