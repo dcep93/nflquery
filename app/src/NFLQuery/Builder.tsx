1786984a799318a31279620605c2bb30e1f75cd6
dcep93 <dcep93@gmail.com>
2025-01-28 23:14:21 -0500

    x

--- app/src/NFLQuery/Builder.tsx
+++ app/src/NFLQuery/Builder.tsx
@@ -1,5 +1,5 @@
 import { DataType, DriveType, GameType, PlayType } from "./Data";
-import { clockToSeconds, getHomeAdvantage, groupByF, PointType } from "./Query";
+import { groupByF, PointType } from "./Query";
 
 export type BuilderType = {
   d: DataType;
@@ -9,73 +9,6 @@ export type BuilderType = {
   p: PlayType;
   pi: number;
 };
-
-// todo
-export function BestInTime<T>(args: {
-  transform: (o: BuilderType) => T;
-  filter: (o: T) => boolean;
-  map: (o: T) => PointType | null;
-  datas: DataType[];
-}): PointType[] {
-  return args.datas
-    .flatMap((d) =>
-      d.games
-        .map((g) => ({ g, endHomeAdvantage: getHomeAdvantage(g.scores) }))
-        .flatMap(({ g, endHomeAdvantage }) =>
-          g.drives.map((dr, i) => ({
-            d,
-            g,
-            dr,
-            elapsedSeconds: clockToSeconds(
-              dr.plays
-                .slice()
-                .reverse()
-                .find((p) => p.type !== "TD")!.clock
-            ),
-            x: dr.plays[dr.plays.length - 1].clock,
-            y: (i === 0
-              ? null
-              : endHomeAdvantage === 0
-              ? null
-              : getHomeAdvantage(g.drives[i - 1].scores) *
-                (endHomeAdvantage > 0 ? -1 : 1))!,
-            label: `${
-              g.scores[0] > g.scores[1]
-                ? g.teams.map((t) => t.name).join(" @ ")
-                : g.teams
-                    .slice()
-                    .reverse()
-                    .map((t) => t.name)
-                    .join(" vs ")
-            } ${d.year}w${g.week}:${g.gameId}`,
-          }))
-        )
-    )
-    .filter((o) => o.y > 0)
-    .sort((a, b) =>
-      a.elapsedSeconds === b.elapsedSeconds
-        ? b.y - a.y
-        : b.elapsedSeconds - a.elapsedSeconds
-    )
-    .reduce(
-      (prev, curr) =>
-        prev.record >= curr.y
-          ? prev
-          : {
-              record: curr.y,
-              rval: prev.rval.concat({
-                x: curr.x,
-                y: curr.y,
-                label: curr.label,
-              }),
-            },
-      {
-        record: -1,
-        rval: [] as PointType[],
-      }
-    )
-    .rval.sort((a, b) => b.y - a.y);
-}
 
 export function MaxBuilder<T>(args: {
   transform: (o: BuilderType) => T;
