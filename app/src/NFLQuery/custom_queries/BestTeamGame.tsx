be636e0a68247ca9ee57ee0acbb50801df63c3e7
Daniel Cepeda <dcep93@gmail.com>
2025-08-10 22:48:33 -0400

    x

--- app/src/NFLQuery/custom_queries/BestTeamGame.tsx
+++ app/src/NFLQuery/custom_queries/BestTeamGame.tsx
@@ -1,3 +1,41 @@
-export default function BestTeamGame() {
-  return <div>BestTeamGame</div>;
+import { MaxBuilder } from "../Builder";
+import { DataType } from "../Data";
+
+export default function BestTeamGame(props: { datas: DataType[] }) {
+  const points = MaxBuilder({
+    transform: (o) => o,
+    filter: ({ dri, pi }) => dri <= 1 && pi === 0,
+    map: ({ d, g, dri }) =>
+      ((o) =>
+        o.punts.length === 0
+          ? null
+          : {
+              x: o.punts.join(","),
+              y: o.punts.reduce((a, b) => a + b, 0) / o.punts.length,
+              label: `${o.matchup} ${d.year}w${g.week}:${g.gameId}`,
+            })({
+        punts: g.drives
+          .filter((d) => d.team === g.teams[dri].name)
+          .flatMap((d) => d.plays)
+          .map((p) => p.text.match(/punts (\d+) yard/))
+          .filter((match) => match)
+          .map((match) => parseInt(match![1])),
+        matchup:
+          dri === 0
+            ? g.teams.map((t) => t.name).join(" @ ")
+            : g.teams
+                .slice()
+                .reverse()
+                .map((t) => t.name)
+                .join(" vs "),
+      }),
+    ...props,
+  });
+  return (
+    <div>
+      <pre style={{ whiteSpace: "pre-wrap" }}>
+        {JSON.stringify(points, null, 2)}
+      </pre>
+    </div>
+  );
 }
--- app/src/NFLQuery/custom_queries/index.tsx
+++ app/src/NFLQuery/custom_queries/index.tsx
@@ -1,12 +1,23 @@
 import { useState } from "react";
+import Data, { DataType } from "../Data";
+import { allYears } from "../Fetch";
 import BestTeamGame from "./BestTeamGame";
 
 const allQueryTypes = { BestTeamGame };
 
+var initialized = false;
+
 export default function CustomQuery() {
+  const [datas, updateDatas] = useState<DataType[] | null>(null);
+  if (!initialized) {
+    initialized = true;
+    Data(allYears).then(updateDatas);
+  }
   const [queryType, updateQueryType] = useState<string>(
-    window.location.hash.slice(1) || Object.keys(allQueryTypes)[0]
+    window.location.hash.slice(1).split(".")[0] || Object.keys(allQueryTypes)[0]
   );
+  window.location.hash = queryType;
+  if (!datas) return <div></div>;
   const Element = allQueryTypes[queryType as keyof typeof allQueryTypes];
   return (
     <div>
@@ -26,7 +37,9 @@ export default function CustomQuery() {
             ))}
           </select>
         </div>
-        <pre style={{ whiteSpace: "pre-wrap" }}>{<Element />}</pre>
+        <pre style={{ whiteSpace: "pre-wrap" }}>
+          {<Element datas={datas} />}
+        </pre>
       </div>
     </div>
   );
