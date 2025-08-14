822bd1cb7dac914fc266a5430fb2ed553c2ad403
Daniel Cepeda <dcep93@gmail.com>
2025-08-13 20:43:18 -0700

    x

--- .github/workflows/build_react.sh
+++ .github/workflows/build_react.sh
@@ -5,8 +5,6 @@ set -xeuo pipefail
 # npx create-react-app app --template typescript
 
 cd app
-npm config set legacy-peer-deps true
-yarn install --save-dev ajv@^7
 yarn install
 yarn build
 rm -rf node_modules
--- .github/workflows/delete_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-# npx create-react-app app --template typescript
-
-cd app
-rm -rf NFLQuery/data_v1
