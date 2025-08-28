1aef8184e2bc0e258c548df88c41b3c04ea80704
Daniel Cepeda <dcep93@gmail.com>
2025-08-28 16:29:02 -0400

    x

--- app/src/NFLQuery/FantasyTrends.tsx
+++ app/src/NFLQuery/FantasyTrends.tsx
@@ -1,20 +1,13 @@
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import Data, { DataType } from "./Data";
 import { datasToPlayerYearScores } from "./Fantasy";
 import { allYears } from "./Fetch";
-
-var initialized = false;
 
 // player: (year)(traded)(consistent/died/injured)[]
 
 export default function Trends() {
   const [datas, updateData] = useState<DataType[] | null>(null);
-  useMemo(() => {
-    if (!initialized) {
-      initialized = true;
-      Data(allYears).then(updateData);
-    }
-  }, []);
+  Data(allYears).then(updateData);
   return datas === null ? null : (
     <pre>
       {JSON.stringify(
