78321c8fa09ba38267051e69f55b99629eb9a141
Daniel Cepeda <dcep93@gmail.com>
2025-08-28 19:22:53 -0400

    x

--- app/src/NFLQuery/FantasyTrends.tsx
+++ app/src/NFLQuery/FantasyTrends.tsx
@@ -1,30 +1,45 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import Data, { DataType } from "./Data";
-import { datasToPlayerYearScores, FantasyYear } from "./Fantasy";
+import { datasToPlayerYearScores } from "./Fantasy";
 import { allYears } from "./Fetch";
 
-// player: (year)(traded)(consistent/died/injured)[]
+const MIN_YEARS_EXP = 6;
+const MIN_BEST_SCORE = 200;
+
+const start = Date.now();
 
 export default function Trends() {
   const [datas, updateData] = useState<DataType[] | null>(null);
-  Data(allYears).then(updateData);
-  return datas === null ? null : (
+  useMemo(() => {
+    Data(allYears).then(updateData);
+  }, []);
+  if (datas === null) return null;
+  const output = datasToPlayerYearScores(datas)
+    .filter((o) => o.years.length >= MIN_YEARS_EXP)
+    .map((o) => ({
+      ...o,
+      max: o.years.map((y) => y.total).sort((a, b) => b - a)[0],
+    }))
+    .filter((o) => o.max >= MIN_BEST_SCORE)
+    .sort((a, b) => b.max - a.max)
+    .map((o) => ({
+      ...o,
+      years: o.years.map(
+        (year) =>
+          year.scores
+            .slice(0, -2)
+            .sort((a, b) => a - b)
+            .map((v, i) => ({ v, i }))
+            .find(({ v }) => v > 0)?.i || -1
+      ),
+    }));
+  return (
     <pre>
       {JSON.stringify(
-        datasToPlayerYearScores(datas.filter((d) => d.year >= 2020))
-          .filter((p) => p.name.includes("Amon"))
-          .map(({ name, position, years }) => ({
-            name,
-            position,
-            years: years.map((year) => classify(year)),
-          })),
+        { count: output.length, duration: Date.now() - start, output },
         null,
         2
       )}
     </pre>
   );
-}
-
-function classify(year: FantasyYear): string {
-  return JSON.stringify(year);
 }
