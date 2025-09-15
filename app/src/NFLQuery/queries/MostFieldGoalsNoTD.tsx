876f308519ea7dead1fa86e5e694416873be8dbe
Daniel Cepeda <dcep93@gmail.com>
2025-09-14 23:21:25 -0400

    x

--- app/src/NFLQuery/queries/MostFieldGoalsNoTD.tsx
+++ app/src/NFLQuery/queries/MostFieldGoalsNoTD.tsx
@@ -7,15 +7,20 @@ export default BuildQueryConfig({
       o.teamIndex !== 0
         ? []
         : o.g.drives.find(
-              (dr) => dr.plays.find((p) => p.type === "TD") !== undefined
+              (dr) =>
+                dr.result === "Touchdown" ||
+                dr.result === "TD" ||
+                dr.plays.find((p) => p.type === "TD") !== undefined
             ) !== undefined
           ? []
           : [o.g],
     mapPoints: (points) =>
       points.map((o) => ({
         x: "x",
-        y: o.extraction.drives.filter(
-          (dr) => dr.plays.find((p) => p.type === "FG") !== undefined
+        y: -o.extraction.drives.filter(
+          (dr) =>
+            dr.result?.includes("Field Goal") ||
+            dr.plays.find((p) => p.type === "FG") !== undefined
         ).length,
         label: o.label,
       })),
